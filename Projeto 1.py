26e4a8795ecaf4f136d27d43018ce1362d6d385e
Davi Viana Gouveia <davivianagouveiaPOLI@gmail.com>
2023-07-18 19:36:56 -0300

    Update Projeto 1.py
    adicionando um grafico de  Preço médio de sapatos para as 15 principais marcas

--- Projeto 1.py
+++ Projeto 1.py
@@ -14,33 +14,17 @@ import matplotlib.pyplot as plt
 #Carregando o arquivo dataset
 df = pd.read_csv(r'C:\Users\daviv\OneDrive\Área de Trabalho\pyton\archive\Datafiniti_Womens_Shoes.csv')
 
-top_brands = list(df['brand'].value_counts().head(30).index)
-top_30 = df[df['brand'].isin(top_brands)]
-
-mean_price = top_30.groupby('brand')[['prices']].mean().sort_values(by = 'prices', ascending = False)
-mean_price = mean_price.reset_index()
-
-mean_price.head()
-
 x=df2.nlargest(15,'prices.amountMax')
 df2 = df.drop_duplicates(subset=['brand'])
 
-
-
-
-
-df.columns
-
 plt.figure(figsize=(17, 8))
-plt.barh(df['brand'],df['prices.amountMax'])
-plt.xlabel('Ocorrência de Fatalidades')
-plt.title('valor/dolar')
+plt.barh(x['brand'],x['prices.amountMax'])
+plt.xlabel('valor em dolar')
+plt.title('Preço médio de sapatos para as 15 principais marcas')
 plt.show()
 
-df[['prices.amountMin', 'prices.amountMax']].describe()
 
-df = df[['brand', 'categories', 'dateAdded' , 'colors', 'name', 'imageURLs',
-       'prices.amountMax', 'prices.color', 'prices.currency']]
 
-df.head()
+
+
 
