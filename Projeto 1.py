4578b2ac6dafe4fd6cf24a474b61c7628bdcb808
Davi Viana Gouveia <davivianagouveiaPOLI@gmail.com>
2023-07-18 18:34:10 -0300

    Update Projeto 1.py
    retirei os testes  e adicionei os codigos pra assim fazer uma base de marcas e valor maximo

--- Projeto 1.py
+++ Projeto 1.py
@@ -14,53 +14,16 @@ import matplotlib.pyplot as plt
 #Carregando o arquivo dataset
 df = pd.read_csv(r'C:\Users\daviv\OneDrive\Área de Trabalho\pyton\archive\Datafiniti_Womens_Shoes.csv')
 
-print(f'Dataset Shape : {df.shape}')
-df.head()
+top_brands = list(df['brand'].value_counts().head(30).index)
+top_30 = df[df['brand'].isin(top_brands)]
 
-df.info()
+mean_price = top_30.groupby('brand')[['prices']].mean().sort_values(by = 'prices', ascending = False)
+mean_price = mean_price.reset_index()
 
-nans = df.isna().sum().sort_values(ascending = False)
+mean_price.head()
 
-pct = 100*nans/df.shape[0]
-missing = pd.concat([nans, pct], axis = 1).rename({0 : 'Num of missing values', 1 : 'Percentage of missing values'}, axis = 1)
-
-missing.head(20)
-
-df[['prices.amountMin', 'prices.amountMax']].describe()
-
-df = df[['brand', 'categories', 'dateAdded' , 'colors', 'name', 'imageURLs',
-       'prices.amountMax', 'prices.color', 'prices.currency',]]
-
-df.head()
-
-df['brand'] = df['brand'].str.lower().str.strip().str.replace('@', 'a',  regex=True)
-
-
-brands = ['aerosoles', 'kenneth cole', 'ellie', 'toms', 'ugg', 'pleaser', 'michael kors', 'ralph lauren',
-          'puma', 'walking cradles', 'vionic', 'vince', 'victoria', 'valentino', 'adidas', 'bcbg', 'sperry',
-          'spring step', 'nina' , 'moda'
-         ]
-
-for brand in  brands :
-    filt = df['brand'].str.contains(brand).fillna(False)
-    df.loc[filt, 'brand'] = brand
-
-non_brands = ['famous name brand', 'generic', 'generic surplus', 'non-branded',
-             'not applicable', 'not rated', 'lucky  brand', 'lucky brand', 'very fine dance shoes'
-             ]
-filt = df['brand'].isin(non_brands)
-df.loc[filt, 'brand'] = 'unbranded'
-df['brand'].fillna('unbranded', inplace = True)
-
-ls = df['brand'].value_counts()[df['brand'].value_counts() < 20]
-ls = list(ls.index)
-filt = df['brand'].isin(ls)
-df.loc[filt, 'brand'] = 'small brands'
-
-
-df['brand'] = df['brand'].str.capitalize()
-
-df['brand'].value_counts()
+x=df2.nlargest(15,'prices.amountMax')
+df2 = df.drop_duplicates(subset=['brand'])
 
 
 
